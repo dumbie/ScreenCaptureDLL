943496c284fd725bec49b5e8a86e9c0ac7f8bb54
Arnold Vink <info@arnoldvink.com>
2023-04-19 12:23:13 +0200

    - Removed ScreenCaptureNow, replaced by ScreenCaptureTool.

--- ScreenCaptureNow/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-//General Information about assembly
-[assembly: AssemblyTitle("Screen Capture Now")]
-[assembly: AssemblyProduct("Screen Capture Now")]
-[assembly: AssemblyDescription("Screen Capture Now")]
-[assembly: AssemblyCompany("Arnold Vink")]
-[assembly: AssemblyCopyright("Copyright © Arnold Vink")]
