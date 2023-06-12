a2efa4021948e3a115615b98adb177f3d4c44a0d
Arnold Vink <info@arnoldvink.com>
2023-06-12 12:12:48 +0200

    - Changed code that awaits audio playback.

--- ScreenCapture/PlayAudio.cpp
+++ ScreenCapture/PlayAudio.cpp
@@ -1,14 +1,22 @@
 #pragma once
 #include "CaptureIncludes.h"
+#include "CaptureVariables.h"
 
 namespace
 {
 	VOID PlayAudio(WCHAR* audioFile)
 	{
+		//Check settings
+		if (!vCaptureSettings.SoundEffect)
+		{
+			return;
+		}
+
 		//Set resources
 		CComPtr<IGraphBuilder> iGraphBuilder;
 		CComPtr<IMediaControl> iMediaControl;
 		CComPtr<IMediaSeeking> iMediaSeeking;
+		CComPtr<IMediaEvent> iMediaEvent;
 		try
 		{
 			//Create graph builder
@@ -17,22 +25,45 @@ namespace
 			//Convert variables
 			iGraphBuilder->QueryInterface(&iMediaControl);
 			iGraphBuilder->QueryInterface(&iMediaSeeking);
+			iGraphBuilder->QueryInterface(&iMediaEvent);
 
 			//Load audio file
-			iGraphBuilder->RenderFile(audioFile, NULL);
+			hResult = iGraphBuilder->RenderFile(audioFile, NULL);
+			if (FAILED(hResult))
+			{
+				std::wcout << "Render audio file failed: " << audioFile << std::endl;
+				return;
+			}
 
 			//Play audio file
-			iMediaControl->Run();
+			hResult = iMediaControl->Run();
+			if (FAILED(hResult))
+			{
+				std::wcout << "Run audio file failed: " << audioFile << std::endl;
+				return;
+			}
 
-			//Wait for completion
+			//Get audio duration
 			LONGLONG audioDuration;
 			iMediaSeeking->GetDuration(&audioDuration);
-			Sleep(audioDuration / 10000);
+			LONGLONG audioDurationMs = audioDuration / 10000;
+			if (audioDurationMs < 2500)
+			{
+				audioDurationMs = 2500;
+			}
+
+			//Wait for completion
+			long eventCode;
+			iMediaEvent->WaitForCompletion(audioDurationMs, &eventCode);
 		}
-		catch (...) {}
+		catch (...)
+		{
+			std::wcout << L"Failed to play audio file: " << audioFile << std::endl;
+		}
 		//Release resources
 		iGraphBuilder.Release();
 		iMediaControl.Release();
 		iMediaSeeking.Release();
+		iMediaEvent.Release();
 	}
 }
