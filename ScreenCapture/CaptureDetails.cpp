76512e182640a3b27420ffdcc71c9b2eb10d9ea3
Arnold Vink <vink_arnold@hotmail.com>
2023-11-10 15:28:19 +0100

    - Updated code to get SDR white level by HMONITOR / path info.

--- ScreenCapture/CaptureDetails.cpp
+++ ScreenCapture/CaptureDetails.cpp
@@ -3,6 +3,62 @@
 
 namespace
 {
+	BOOL GetMonitorPathInfo(HMONITOR hMonitor, DISPLAYCONFIG_PATH_INFO* pathInfo)
+	{
+		try
+		{
+			MONITORINFOEXW monitorInfo;
+			monitorInfo.cbSize = sizeof(monitorInfo);
+			if (!GetMonitorInfoW(hMonitor, &monitorInfo))
+			{
+				return false;
+			}
+
+			UINT displayPathCount;
+			UINT displayModeCount;
+			hResult = GetDisplayConfigBufferSizes(QDC_ONLY_ACTIVE_PATHS, &displayPathCount, &displayModeCount);
+			if (FAILED(hResult))
+			{
+				return false;
+			}
+
+			std::vector<DISPLAYCONFIG_PATH_INFO> displayPathInfos(displayPathCount);
+			std::vector<DISPLAYCONFIG_MODE_INFO> displayModeInfos(displayModeCount);
+			hResult = QueryDisplayConfig(QDC_ONLY_ACTIVE_PATHS, &displayPathCount, displayPathInfos.data(), &displayModeCount, displayModeInfos.data(), NULL);
+			if (FAILED(hResult))
+			{
+				return false;
+			}
+
+			for (DISPLAYCONFIG_PATH_INFO pathInfoLoop : displayPathInfos)
+			{
+				DISPLAYCONFIG_SOURCE_DEVICE_NAME sourceName{};
+				sourceName.header.size = sizeof(sourceName);
+				sourceName.header.type = DISPLAYCONFIG_DEVICE_INFO_GET_SOURCE_NAME;
+				sourceName.header.adapterId = pathInfoLoop.targetInfo.adapterId;
+				sourceName.header.id = pathInfoLoop.sourceInfo.id;
+				hResult = DisplayConfigGetDeviceInfo(&sourceName.header);
+				if (FAILED(hResult))
+				{
+					continue;
+				}
+
+				if (wcscmp(monitorInfo.szDevice, sourceName.viewGdiDeviceName) != 0)
+				{
+					continue;
+				}
+
+				*pathInfo = pathInfoLoop;
+				return true;
+			}
+		}
+		catch (...)
+		{
+			std::cout << "Failed to get monitor path info." << std::endl;
+		}
+		return false;
+	}
+
 	void GetMonitorResolution(HMONITOR hMonitor, UINT& pixelsWidth, UINT& pixelsHeight)
 	{
 		try
@@ -57,46 +113,117 @@ namespace
 		}
 	}
 
-	FLOAT GetMonitorSDRWhiteLevel(UINT monitorId)
+	FLOAT GetMonitorSDRWhiteLevel(HMONITOR hMonitor)
 	{
 		try
 		{
-			UINT displayPathCount;
-			UINT displayModeCount;
-			hResult = GetDisplayConfigBufferSizes(QDC_ONLY_ACTIVE_PATHS, &displayPathCount, &displayModeCount);
-			if (FAILED(hResult))
+			//Get display path info
+			DISPLAYCONFIG_PATH_INFO pathInfo;
+			if (!GetMonitorPathInfo(hMonitor, &pathInfo))
 			{
+				std::cout << "Failed to get monitor sdr white level." << std::endl;
 				return 240.0F;
 			}
 
-			DISPLAYCONFIG_PATH_INFO* pathInfos = new DISPLAYCONFIG_PATH_INFO[displayPathCount];
-			DISPLAYCONFIG_MODE_INFO* modeInfos = new DISPLAYCONFIG_MODE_INFO[displayModeCount];
-			hResult = QueryDisplayConfig(QDC_ONLY_ACTIVE_PATHS, &displayPathCount, pathInfos, &displayModeCount, modeInfos, NULL);
-			if (FAILED(hResult))
-			{
-				return 240.0F;
-			}
-
-			DISPLAYCONFIG_PATH_INFO pathInfo = pathInfos[monitorId];
-
+			//Get display sdr white level
 			DISPLAYCONFIG_SDR_WHITE_LEVEL sdrWhiteLevel{};
 			sdrWhiteLevel.header.size = sizeof(sdrWhiteLevel);
 			sdrWhiteLevel.header.type = DISPLAYCONFIG_DEVICE_INFO_GET_SDR_WHITE_LEVEL;
 			sdrWhiteLevel.header.adapterId = pathInfo.targetInfo.adapterId;
 			sdrWhiteLevel.header.id = pathInfo.targetInfo.id;
-
 			hResult = DisplayConfigGetDeviceInfo(&sdrWhiteLevel.header);
 			if (FAILED(hResult))
 			{
+				std::cout << "Failed to get monitor sdr white level." << std::endl;
 				return 240.0F;
 			}
-
-			return (sdrWhiteLevel.SDRWhiteLevel / 1000.0F) * 80.0F;
+			else
+			{
+				return (sdrWhiteLevel.SDRWhiteLevel / 1000.0F) * 80.0F;
+			}
 		}
 		catch (...)
 		{
 			std::cout << "Failed to get monitor sdr white level." << std::endl;
 			return 240.0F;
+		}
+	}
+
+	BOOL SetCaptureDetails()
+	{
+		try
+		{
+			//Get and set HDR details
+			vCaptureDetails.HDREnabled = vDirectXInstance.iDxgiOutputDescription1.ColorSpace == DXGI_COLOR_SPACE_RGB_FULL_G2084_NONE_P2020 || vDirectXInstance.iDxgiOutputDescription1.ColorSpace == DXGI_COLOR_SPACE_RGB_STUDIO_G2084_NONE_P2020;
+			if (vCaptureDetails.HDREnabled)
+			{
+				vCaptureDetails.HDRtoSDR = vCaptureSettings.HDRtoSDR;
+				if (vCaptureDetails.HDRtoSDR)
+				{
+					vCaptureInstance.iWicPixelFormatGuidSource = GUID_WICPixelFormat32bppBGRA;
+					vCaptureInstance.vCaptureDxgiFormat = DXGI_FORMAT_B8G8R8A8_UNORM; //DXGI_FORMAT_B8G8R8A8_UNORM_SRGB
+					vCaptureDetails.PixelByteSize = 4;
+					vCaptureDetails.SDRWhiteLevel = GetMonitorSDRWhiteLevel(vDirectXInstance.iDxgiOutputDescription1.Monitor);
+				}
+				else
+				{
+					vCaptureInstance.iWicPixelFormatGuidSource = GUID_WICPixelFormat64bppRGBAHalf;
+					vCaptureInstance.vCaptureDxgiFormat = DXGI_FORMAT_R16G16B16A16_FLOAT;
+					vCaptureDetails.PixelByteSize = 8;
+				}
+			}
+			else
+			{
+				vCaptureDetails.HDRtoSDR = false;
+				vCaptureInstance.iWicPixelFormatGuidSource = GUID_WICPixelFormat32bppBGRA;
+				vCaptureInstance.vCaptureDxgiFormat = DXGI_FORMAT_B8G8R8A8_UNORM;
+				vCaptureDetails.PixelByteSize = 4;
+			}
+
+			//Get monitor details
+			UINT monitorPixelsWidth = 0;
+			UINT monitorPixelsHeight = 0;
+			UINT monitorRefreshRate = GetMonitorRefreshRate(vDirectXInstance.iDxgiOutputDescription1.Monitor);
+			GetMonitorResolution(vDirectXInstance.iDxgiOutputDescription1.Monitor, monitorPixelsWidth, monitorPixelsHeight);
+
+			//Update capture variables
+			vCaptureDetails.OriginalWidth = monitorPixelsWidth;
+			vCaptureDetails.OriginalHeight = monitorPixelsHeight;
+			vCaptureDetails.RefreshRate = monitorRefreshRate;
+			vCaptureInstance.vCaptureTextureResizing = vCaptureSettings.MaxPixelDimension != 0 && vCaptureDetails.OriginalWidth > vCaptureSettings.MaxPixelDimension && vCaptureDetails.OriginalHeight > vCaptureSettings.MaxPixelDimension;
+			if (vCaptureInstance.vCaptureTextureResizing)
+			{
+				DOUBLE resizedWidth = 0.01;
+				DOUBLE resizedHeight = 0.01;
+				UINT minDimension = min(vCaptureDetails.OriginalWidth, vCaptureDetails.OriginalHeight);
+				UINT maxDimension = vCaptureSettings.MaxPixelDimension;
+				//Find nearest full pixel dimensions to keep original ratio
+				while (resizedWidth != (UINT)resizedWidth || resizedHeight != (UINT)resizedHeight)
+				{
+					DOUBLE differenceRatio = (DOUBLE)minDimension / maxDimension;
+					resizedWidth = vCaptureDetails.OriginalWidth / differenceRatio;
+					resizedHeight = vCaptureDetails.OriginalHeight / differenceRatio;
+					maxDimension++;
+				}
+				vCaptureDetails.OutputWidth = resizedWidth;
+				vCaptureDetails.OutputHeight = resizedHeight;
+			}
+			else
+			{
+				vCaptureDetails.OutputWidth = vCaptureDetails.OriginalWidth;
+				vCaptureDetails.OutputHeight = vCaptureDetails.OriginalHeight;
+			}
+			std::cout << "Screen capture output, Width: " << vCaptureDetails.OutputWidth << " Height: " << vCaptureDetails.OutputHeight << std::endl;
+			vCaptureDetails.WidthByteSize = vCaptureDetails.OutputWidth * vCaptureDetails.PixelByteSize;
+			vCaptureDetails.TotalByteSize = vCaptureDetails.OutputWidth * vCaptureDetails.OutputHeight * vCaptureDetails.PixelByteSize;
+			vCaptureInstance.vCaptureTextureMipLevels = 1 + log2(max(vCaptureDetails.OutputWidth, vCaptureDetails.OutputHeight));
+
+			return true;
+		}
+		catch (...)
+		{
+			std::cout << "SetCaptureDetails failed: " << hResult << std::endl;
+			return false;
 		}
 	}
 };
