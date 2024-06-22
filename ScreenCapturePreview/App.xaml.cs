f046eadf3448b8cc343b1a1b1a83b728752ad5d5
Arnold Vink <vink_arnold@hotmail.com>
2024-06-22 12:25:54 +0200

    - Release build load dll on startup.

--- ScreenCapturePreview/App.xaml.cs
+++ ScreenCapturePreview/App.xaml.cs
@@ -1,6 +1,19 @@
 ﻿using System.Windows;
+using static ArnoldVinkCode.AVInteropDll;
+using static ArnoldVinkCode.AVStartup;
 
 namespace ScreenCapture
 {
-    public partial class App : Application { }
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            try
+            {
+                //Setup application defaults
+                SetupDefaults(ProcessPriority.Normal, false);
+            }
+            catch { }
+        }
+    }
 }
